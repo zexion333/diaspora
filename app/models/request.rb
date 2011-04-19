5f0d3b6dd608e5247569e0bc64d6ee1af9a831f9
danielgrippi <daniel@joindiaspora.com>
2011-04-18 18:25:19 -0700

    add nonce in receive_tokens

--- app/models/request.rb
+++ app/models/request.rb
@@ -79,7 +79,7 @@ class Request
 
     client = Rack::OAuth2::Client.new(
       :identifier => "#{sender.diaspora_handle};#{recipient.diaspora_handle}",
-      :secret => Base64.encode64("#{time.to_i};#{sig}"),
+      :secret => Base64.encode64("#{time.to_i};#{nonce};#{sig}"),
       #:redirect_uri => YOUR_REDIRECT_URI, # only required for grant_type = :code
       :host => URI.parse(sender.url).host,
       :port => URI.parse(sender.url).port.to_s,
