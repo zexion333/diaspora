c04f49351198bf59fdebf22f0571ebdfba8a7598
danielgrippi <danielgrippi@gmail.com>
2011-08-30 22:03:22 -0700

    added log line

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,6 +5,7 @@ begin
   if redis_to_go = ENV["REDISTOGO_URL"]
     uri = URI.parse(redis_to_go)
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Rails.logger.info("resque connected to #{uri.host} on port #{uri.port}")
   end 
 
   if ENV['SINGLE_PROCESS']
