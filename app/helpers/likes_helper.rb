518bea0aa7f145d4ee6ed0814591965b75cb3edd
Ilya Zhitomirskiy <iz268@nyu.edu>
2011-09-22 18:22:58 -0700

    now comment likes are not hidden if they exist

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -8,22 +8,38 @@ module LikesHelper
     links.join(", ").html_safe
   end
 
-  def like_action(target, current_user=current_user)
-    if target.is_a?(Comment)
-      if current_user.liked?(target)
-        link_to like_heart_red, comment_like_path(target, current_user.like_for(target)), :method => :delete, :class => 'unlike', :remote => true
-      else
-        link_to like_heart, comment_likes_path(target, :positive => 'true'), :method => :post, :class => 'like', :remote => true
-      end
-
+  def like_action(target, current_user=current_user, opts= {})
+    if current_user.liked?(target)
+      already_liked_link(target, current_user, opts)
     else
-
-      if current_user.liked?(target)
-        link_to like_heart_red, post_like_path(target, current_user.like_for(target)), :method => :delete, :class => 'unlike', :remote => true
-      else
-        link_to like_heart, post_likes_path(target, :positive => 'true'), :method => :post, :class => 'like', :remote => true
-      end
+      not_yet_liked_link(target, current_user, opts)
     end
+  end
+
+  def already_liked_link(target, current_user=current_user, opts= {})
+    link_opts = {:method => :delete, :class => 'unlike', :remote => true}
+    link_opts[:class] << " #{opts[:class]}" if opts[:class]
+
+    if target.is_a?(Comment)
+      link_target = comment_like_path(target, current_user.like_for(target))
+    else
+      link_target = post_like_path(target, current_user.like_for(target))
+    end
+
+    link_to like_heart_red, link_target, link_opts
+  end
+
+  def not_yet_liked_link(target, current_user=current_user, opts= {})
+    link_opts = {:method => :post, :class => 'like', :remote => true}
+    link_opts[:class] << " #{opts[:class]}" if opts[:class]
+
+    if target.is_a?(Comment)
+      link_target = comment_likes_path(target, :positive => 'true')
+    else
+      link_target = post_likes_path(target, :positive => 'true')
+    end
+    
+    link_to like_heart, link_target, link_opts
   end
 
   def like_heart
