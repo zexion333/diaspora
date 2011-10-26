b4f9752b9e0a26d5432203febb093b01fcad8184
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-10-26 00:36:14 -0700

    remove a duplicate file, and a pp

--- app/controllers/o_embed_caches_controller.rb
+++ app/controllers/o_embed_caches_controller.rb
@@ -1,7 +1,6 @@
 class OEmbedCachesController < ApplicationController
   def show
     @oembed = OEmbedCache.find(params[:id])
-    pp @oembed
     render :layout => false
   end
 end
--- o_embeds_controller.rb
+++
@@ -1,6 +0,0 @@
-class OEmbedCachesController < ApplicationController
-  def show
-    @oembed = OEmbedCache.find(params[:id])
-  end
-end
-
